12279f8ba5df9d3ef8ab1a108c0567813c151fff
Kateryna2882 <111122172+Kateryna2882@users.noreply.github.com>
2023-11-09 19:02:31 +0200

    Add fields and methods

--- src/ArrayDataException.java
+++ src/ArrayDataException.java
@@ -1,2 +1,6 @@
-public class ArrayDataException {
+public class ArrayDataException extends Exception {
+    public ArrayDataException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
 }
